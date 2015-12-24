95f28d1435befa24d1d38ff9bb03f8ce4226e88f
Joe Martella <martellaj@live.com>
2015-12-23 21:00:23 -0800

    Changed the add/remove pattern a little bit.

--- race-reportr/public/javascripts/main.js
+++ race-reportr/public/javascripts/main.js
@@ -5,24 +5,28 @@ $(function () {
    * @desc Event handler for clicking the add button in the splits section.
    *       This function adds a new input row to the splits section.
    */
-  $('.splits').on('click', '.split-add-btn', function (event) {
+  $('.splits-section').on('click', '.split-add-btn', function (event) {
     event.preventDefault();
     console.debug('Splits row add button clicked.');
 
     // Get the new row number.
     var newRowNumber = $('.splits').children().length + 1;
-
-    // Remove the add and remove buttons from the last row.
-    $('.split-add-btn').remove();
-    $('.split-sub-btn').remove();
-
-    // Build the new row HTML.
-    var splitRow = '<div class="form-group split-div-' + newRowNumber + '">\n';
-    splitRow += '\t<label for="split-' + newRowNumber + '" style="display: inline">' + newRowNumber + ' </label>\n';
-    splitRow += '\t<input id="split-' + newRowNumber + '" type="text" style="display: inline">\n';
-    splitRow += '\t<input class="square-btn-inline split-add-btn" type="button" value="+">\n';
-    splitRow += '\t<input class="square-btn-inline split-sub-btn" type="button" value="-">\n';
-    splitRow += '</div>\n';
+    
+    // Build the new row HTML.    
+    var splitRow = '';
+    
+    // Include placeholder value if first row.
+    if (newRowNumber === 1) {
+      splitRow = '<div class="form-group split-div-1">';
+      splitRow += '\t<label for="split-1" style="display: inline">1 </label>';
+      splitRow += '\t<input id="split-1" type="text" style="display: inline" placeholder="8:46">';
+      splitRow += '</div>';
+    } else {
+      splitRow = '<div class="form-group split-div-' + newRowNumber + '">\n';
+      splitRow += '\t<label for="split-' + newRowNumber + '" style="display: inline">' + newRowNumber + ' </label>\n';
+      splitRow += '\t<input id="split-' + newRowNumber + '" type="text" style="display: inline">\n';
+      splitRow += '</div>\n';
+    }
 
     // Add the row to the splits section.
     $('.splits').append(splitRow);
@@ -33,25 +37,13 @@ $(function () {
    *       This function removes the last row in the splits section, and adds the
    *       add and remove buttons to the previous row.
    */
-  $('.splits').on('click', '.split-sub-btn', function (event) {
+  $('.splits-section').on('click', '.split-sub-btn', function (event) {
     event.preventDefault();
     console.debug('Splits row subtract button clicked.');
 
     // Gets the last row ID and removes it.
     var lastRow = $('.splits').children().length;
     $('.split-div-' + lastRow).remove();
-
-    // Start building the add and remove buttons HTML.
-    var addRemoveButtons = '\t<input class="square-btn-inline split-add-btn" type="button" value="+">\n';
-
-    // If there is only 1 row left, don't add the subtract button.
-    if (lastRow !== 2) {
-      console.log(lastRow);
-      addRemoveButtons += '\t<input class="square-btn-inline split-sub-btn" type="button" value="-">\n';
-    }
-
-    // Add the add and remove buttons HTML to the new last row.
-    $('.split-div-' + (lastRow - 1)).append(addRemoveButtons);
   });
 
   /**
@@ -66,21 +58,34 @@ $(function () {
     var newRowNumber = $('.pictures').children().length + 1;
 
     // Build the new row HTML.
-    var pictureRow = '<div class="picture-div-' + newRowNumber + '">\n';
-    pictureRow += '\t<div class="form-group">';
-    pictureRow += '\t\t<label for="picture-' + newRowNumber + '-link">picture #' + newRowNumber + ' link</label>\n';
-    pictureRow += '\t\t<input class="longer" id="picture-' + newRowNumber + '-link" type="text">\n';
-    pictureRow += '\t</div>';
-    pictureRow += '\t<div class="form-group">';
-    pictureRow += '\t\t<label for="picture-' + newRowNumber + '-description">picture #' + newRowNumber + ' description</label>\n';
-    pictureRow += '\t\t<input class="longer" id="picture-' + newRowNumber + '-description" type="text">\n';
-    pictureRow += '\t</div>\n';
-    pictureRow += '</div>\n';
+    var pictureRow = '';
+    
+    if (newRowNumber === 1) {
+      pictureRow = '<div class="picture-div-1">\n';
+      pictureRow += '\t<div class="form-group">';
+      pictureRow += '\t\t<label for="picture-1-link">picture #1 link</label>\n';
+      pictureRow += '\t\t<input class="longer" id="picture-1-link" type="text" placeholder="http://i.imgur.com/1brDP61.jpg">\n';
+      pictureRow += '\t</div>';
+      pictureRow += '\t<div class="form-group">';
+      pictureRow += '\t\t<label for="picture-1-description">picture #1 description</label>\n';
+      pictureRow += '\t\t<input class="longer" id="picture-1-description" type="text" placeholder="me at mile 8">\n';
+      pictureRow += '\t</div>\n';
+      pictureRow += '</div>\n';
+    } else {
+      pictureRow = '<div class="picture-div-' + newRowNumber + '">\n';
+      pictureRow += '\t<div class="form-group">';
+      pictureRow += '\t\t<label for="picture-' + newRowNumber + '-link">picture #' + newRowNumber + ' link</label>\n';
+      pictureRow += '\t\t<input class="longer" id="picture-' + newRowNumber + '-link" type="text">\n';
+      pictureRow += '\t</div>';
+      pictureRow += '\t<div class="form-group">';
+      pictureRow += '\t\t<label for="picture-' + newRowNumber + '-description">picture #' + newRowNumber + ' description</label>\n';
+      pictureRow += '\t\t<input class="longer" id="picture-' + newRowNumber + '-description" type="text">\n';
+      pictureRow += '\t</div>\n';
+      pictureRow += '</div>\n';
+    }
 
     // Add the row to the pictures section.
     $('.pictures').append(pictureRow);
-
-    $('.picture-sub-btn').prop('disabled', false);
   });
 
   /**
@@ -94,10 +99,6 @@ $(function () {
     // Gets the last row ID and removes it.
     var lastRow = $('.pictures').children().length;
     $('.picture-div-' + lastRow).remove();
-
-    if (lastRow === 2) {
-      $('.picture-sub-btn').prop('disabled', true);
-    }
   });
   
   /**
@@ -130,8 +131,6 @@ $(function () {
 
     // Add the row to the text-heavy section.
     $('.text-heavy-sections').append(textHeavyRow);
-
-    $('.text-sub-btn').prop('disabled', false);
   });
   
   /**
@@ -145,9 +144,5 @@ $(function () {
     // Gets the last row ID and removes it.
     var lastRow = $('.text-heavy-sections').children().length;
     $('.text-div-' + lastRow).remove();
-
-    if (lastRow === 2) {
-      $('.text-sub-btn').prop('disabled', true);
-    }
   });
 });
