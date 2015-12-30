fa462328793a5327d611282ea672c2190d6cd2a1
Joe Martella <martellaj@live.com>
2015-12-30 11:14:09 -0800

    Set up the preview pane. Need content. :japanese_castle:

--- race-reportr/routes/preview.js
+++ race-reportr/routes/preview.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
   console.log(req.query);
-  res.render('preview');
+  res.render('preview', {
+    previewHtml: '<h2>test</h2>'
+  });
 });
 
 module.exports = router;
